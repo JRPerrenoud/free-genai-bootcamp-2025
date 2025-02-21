9de6d793ea8720be1fd7f3b78f96c379355a0feb
JPerreno <jrperren@users.noreply.github.com>
2025-02-21 11:55:36 -0700

    Cleaning up Project - Remove all un-needed files. Rename lang_portal to lang_portal_go.  Start working with course provided lang_portal using flask. All future changes will go here for lang_portal.
    Signed-off-by: JPerreno <jrperren@users.noreply.github.com>

--- lang-portal-learnings/Backend_go_WinPowerShell/scripts/init_db.ps1
+++
@@ -1,8 +0,0 @@
-# Refresh environment variables
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-
-# Enable CGO
-$env:CGO_ENABLED=1
-
-# Run mage db:init
-mage db:init
--- lang-portal-learnings/lang_portal_adding front_end_first_try/frontend_react/src/types/modules.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'clsx' {
-  export type ClassValue = string | number | boolean | undefined | null | { [key: string]: any } | ClassValue[];
-  export default function clsx(...inputs: ClassValue[]): string;
-}
-
-declare module 'tailwind-merge' {
-  export function twMerge(...inputs: string[]): string;
-}
